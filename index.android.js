ab1831e7afed4d26e9fc930fcb001c2d7a8b341e
Vincent Chann <vincentchann@Mac-mini-de-Vincent.local>
2017-03-09 00:01:05 +0100

    RN-02 : onwards : react components in separate files

--- index.android.js
+++ index.android.js
@@ -1,14 +1,12 @@
 // import library to help create a component
-import React from 'react'; // component behaviour and make them work together
-import { Text, AppRegistry } from 'react-native'; // convert output into native widget display on screen + default widget library
-// import Text and AppRegistry from react native library
+import React from 'react'; 
+import { Text, AppRegistry } from 'react-native'; // destructured import : import Text and AppRegistry from react native library
+import Header from './src/components/header';
 
-
-// create a component is a javascript function that return some amount of JSX
-// => then react native is gonna render that JSX on the screen
+// create a component : javascript function that return some amount of JSX
 const App = () => (
-    <Text>Some Text</Text> // looks like HTML but it's a javascript dialect called JSX that hide javascript called
+    <Header/> // looks like HTML but it's a javascript dialect called JSX that hide javascript called
   );
 
-// render it to the device
-AppRegistry.registerComponent('Project', () => App); // app name & App JSX
+// render it to the device : Appregistry is only used to show the root component, then this root component will display all his childs
+AppRegistry.registerComponent('Project', () => App); // app name & get JSX return by component App
---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+// import libraries for making a component
+import React from 'react';
+import { Text } from 'react-native'; // destructured import
+
+// make a component
+const Header = () => {
+  return (
+    <Text>Albums!</Text>
+  );
+};
+
+// make the component available to other parts of the app
+export default Header;
