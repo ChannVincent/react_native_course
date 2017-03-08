3b8dd0ef653f2f82d009dac993d1a359b8ac2a4c
Vincent Chann <vincentchann@Mac-mini-de-Vincent.local>
2017-03-09 00:34:04 +0100

    RN-02 : onwards : styling depending on platform

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,35 @@
 // import libraries for making a component
 import React from 'react';
-import { Text } from 'react-native'; // destructured import
+import { Platform, Text, View } from 'react-native'; // destructured import
 
 // make a component
 const Header = () => {
+  const { textStyle, viewStyle } = styles; // destructured import => instead of saying styles.viewStyle && styles.textStyle everywhere
+  
   return (
-    <Text>Albums!</Text>
+    <View style={viewStyle}>
+      <Text style={textStyle}>Albums!</Text>
+    </View>
   );
 };
+
+const styles = {
+  viewStyle: {
+    backgroundColor: (Platform.OS === 'ios') ? '#f8f8f8' : '#dddddd',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: (Platform.OS === 'ios') ? 15 : 0,
+    shadowColor: '#000000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position : 'relative'
+  },
+  textStyle: {
+    fontSize: 20
+  }
+}
 
 // make the component available to other parts of the app
 export default Header;
