339d4a47b392337e6940f0099369cc440adc2754
Vincent Chann <vincentchann@Mac-mini-de-Vincent.local>
2017-03-10 00:35:52 +0100

    RN-04 : http request : npm install axios (Install > Install npm module)

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 // we used class based component != functional component
 // because we have dynamic states & different states to render on the screen 
@@ -9,6 +10,8 @@ class AlbumList extends Component {
   componentWillMount() {
     console.log('componentWillMount in AlbumList'); // launch debugger with Cmd+D > debug JS > right click > inspect > Console
     // debugger; // set a break point in the code to stop javascript a see what is going on
+    axios.get('http://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => console.log(response));
   }
   
   // only mandatory method of component class
