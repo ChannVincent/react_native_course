7c7df5a265662482e7347afee70a728bd07b0e3a
Vincent Chann <vincentchann@Mac-mini-de-Vincent.local>
2017-03-10 00:23:24 +0100

    RN-04 : Http request : class component & life cycle & debugger

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>Album List !!!</Text>
-    </View>
-  );
+// we used class based component != functional component
+// because we have dynamic states & different states to render on the screen 
+class AlbumList extends Component {
+  
+  // life cycle method : callback onCreate
+  componentWillMount() {
+    console.log('componentWillMount in AlbumList'); // launch debugger with Cmd+D > debug JS > right click > inspect > Console
+    // debugger; // set a break point in the code to stop javascript a see what is going on
+  }
+  
+  // only mandatory method of component class
+  render() {
+    return (
+      <View>
+        <Text>Album List !!!</Text>
+      </View>
+    );
+  }
+  
 };
 
 export default AlbumList;
