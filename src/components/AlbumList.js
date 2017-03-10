0321c67e80383dcad5bdfc91fdd62ac7e71cc085
Vincent Chann <vincentchann@Mac-mini-de-Vincent.local>
2017-03-10 23:53:52 +0100

    RN-04 : http request : states & map datas

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 // we used class based component != functional component
 // because we have dynamic states & different states to render on the screen 
 class AlbumList extends Component {
   
+  // when you change state with this.setState() it will call again the render callback
+  state = { albums: [] }
+  
   // life cycle method : callback onCreate
   componentWillMount() {
-    console.log('componentWillMount in AlbumList'); // launch debugger with Cmd+D > debug JS > right click > inspect > Console
-    // debugger; // set a break point in the code to stop javascript a see what is going on
     axios.get('http://rallycoding.herokuapp.com/api/music_albums')
-    .then(response => console.log(response));
+    .then(response => this.setState({ albums: response.data }));
+  }
+  
+  // map function : for each album will return a text tag which contains album.title
+  renderAlbums() {
+    return this.state.albums.map(
+      album => <AlbumDetail key={album.title} album={album}/>); // to reference a javascript variable into JSX you must place it into curly braces
   }
   
   // only mandatory method of component class
   render() {
+    console.log(this.state);
     return (
       <View>
-        <Text>Album List !!!</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
