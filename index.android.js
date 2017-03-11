a0a814fb132db832997f445df0fb872d26b569f0
Vincent Chann <vincentchann@Mac-mini-de-Vincent.local>
2017-03-11 15:04:20 +0100

    RN-06 : Authentication with Firebase : refactor index android & iOS

--- index.android.js
+++ index.android.js
@@ -1,18 +1,5 @@
-// import library to help create a component
 import React from 'react'; 
-import { View, AppRegistry } from 'react-native'; // destructured import : import Text and AppRegistry from react native library
-import Header from './src/components/header';
-import AlbumList from './src/components/AlbumList';
+import { AppRegistry } from 'react-native'; 
+import App from './src/App';
 
-// create a component : javascript function that return some amount of JSX
-const App = () => {
-  return (
-  <View style={{ flex: 1 }}>
-    <Header headerText={'Albums'} />
-    <AlbumList/>
-  </View>    
-  );
-}
-
-// render it to the device : Appregistry is only used to show the root component, then this root component will display all his childs
-AppRegistry.registerComponent('Project', () => App); // app name & get JSX return by component App
+AppRegistry.registerComponent('Project', () => App); 
