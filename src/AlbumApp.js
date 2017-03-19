0dc0f7043c1d24588c1544a004a11de1cc174aba
Vincent Chann <vincentchann@Mac-mini-de-Vincent.local>
2017-03-19 22:43:09 +0100

    RN-09 : Rendering list the right way

---
+++ src/AlbumApp.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import { Header } from './components/common';
+import AlbumList from './components/AlbumList';
+
+class App extends Component {
+  
+  render() {
+    return (
+      <View>
+        <Header />
+        <AlbumList />
+      </View>
+    )
+  }
+} 
+
+export default App;
