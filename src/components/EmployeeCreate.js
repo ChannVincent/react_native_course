fbf913afd8ede6dd57a6072fc7050b51b028ac31
Vincent Chann <vincentchann@Mac-mini-de-Vincent.local>
2017-03-26 23:19:21 +0200

    RN-13 : Navigating users around : picker & override styles

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Picker, Text } from 'react-native';
 import { Card, CardSection, Input, Button } from './common';
 import { connect } from 'react-redux';
 import { employeeUpdate } from '../actions';
 
 class EmployeeCreate extends Component {
   render() {
-    const { name, phone, employeeUpdate } = this.props;
+    const { name, phone, employeeUpdate, shift } = this.props;
     return (
       <View>
         <Card>
@@ -29,9 +29,25 @@ class EmployeeCreate extends Component {
           </CardSection>
         
           <CardSection>
+            <Picker
+              style={{ flex: 1 }}
+              selectedValue={ shift }
+              onValueChange={ day => { employeeUpdate({ prop: 'shift', value: day }) } }
+              >
+              <Picker.Item label="Monday" value="Monday" />
+              <Picker.Item label="Tuesday" value="Tuesday" />
+              <Picker.Item label="Wednesday" value="Wednesday" />
+              <Picker.Item label="Thursday" value="Thursday" />
+              <Picker.Item label="Friday" value="Friday" />
+              <Picker.Item label="Saturday" value="Saturday" />
+              <Picker.Item label="Sunday" value="Sunday" />
+            </Picker>
           </CardSection>
         
           <CardSection>
+            <Button>
+              Create
+            </Button>
           </CardSection>
         </Card>
       </View>
@@ -41,11 +57,16 @@ class EmployeeCreate extends Component {
 
 const mapStateToProps = (state, ownProps) => {
   console.log(state.employeeForm);
-  const { name, phone } = state.employeeForm;
+  const { name, phone, shift } = state.employeeForm;
   return {
     name,
-    phone
+    phone,
+    shift
   }
+}
+
+const styles = {
+  
 }
 
 export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
