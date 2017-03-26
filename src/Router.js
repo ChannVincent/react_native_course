03eadb825c9ea06102bfa0139841597be7b9b751
Vincent Chann <vincentchann@Mac-mini-de-Vincent.local>
2017-03-26 20:33:20 +0200

    RN-13 : Navigating users around : navigating between employee list and employee create

--- src/Router.js
+++ src/Router.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux'
+import { Scene, Router, Actions } from 'react-native-router-flux'
 import { Platform } from 'react-native'
 import LoginForm from './components/LoginForm'
 import EmployeeList from './components/EmployeeList'
+import EmployeeCreate from './components/EmployeeCreate'
 
 const RouterComponent = () => {
     return (
@@ -12,14 +13,20 @@ const RouterComponent = () => {
             key='login'
             component={ LoginForm }
             title='Please Login'
-            initial
             />
         </Scene>
-        <Scene key='main'>
+        <Scene key='main' initial >
           <Scene 
             key='employeeList'
             component={ EmployeeList }
             title='Employees'
+            onRight={ () => { Actions.employeeCreate() } }
+            rightTitle='Add'
+            />
+          <Scene
+            key='employeeCreate'
+            title='Create employee'
+            component={ EmployeeCreate }
             />
         </Scene>
       </Router>
